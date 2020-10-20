a4f093e92c6987dd273cb7857b2313f59d3ad9d2
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-10-20 18:27:10 +0200

    Clean up appbars package
    Co-authored-by: David Dupraz <duprazdavid@gmail.com>

--- src/mobile/src/main/java/tupperdate/android/appbars/mainTopBar.kt
+++ src/mobile/src/main/java/tupperdate/android/appbars/mainTopBar.kt
@@ -1,27 +1,11 @@
 package tupperdate.android.appbars
 
-import androidx.compose.foundation.Icon
-import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.IconButton
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.style.TextAlign
-import tupperdate.android.R
+/*
+Commented because it doesn't find Modifier.weight, which however works in Onboarding.kt...
 
 @Composable
 fun mainTopBar() {
-    //commented because it doesn't find Modifier.weight, which however works in Onboarding.kt...
-    /*TopAppBar(
+    TopAppBar(
         title = {
             Column(modifier = Modifier.weight(1f), horizontalGravity = Alignment.CenterHorizontally) {
                 Text(text = "tupper.date", textAlign = TextAlign.Center)
@@ -38,5 +22,6 @@ fun mainTopBar() {
             }
         },
         backgroundColor = Color.White
-    )*/
+    )
 }
+*/
