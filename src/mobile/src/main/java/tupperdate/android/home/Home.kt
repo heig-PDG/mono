2560b9f848c846001923c96df1e159f262b18403
David <duprazdavid@gmail.com>
2020-11-01 16:39:23 +0100

    First recipe card

--- src/mobile/src/main/java/tupperdate/android/home/Home.kt
+++ src/mobile/src/main/java/tupperdate/android/home/Home.kt
@@ -1,83 +1,65 @@
 package tupperdate.android.home
 
-import android.view.Gravity
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.onDispose
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.gesture.scrollorientationlocking.Orientation
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import tupperdate.android.appbars.mainBottomBar
 import tupperdate.android.appbars.mainTopBar
 import tupperdate.android.ui.TupperdateTheme
-import tupperdate.api.AuthenticationApi
-import tupperdate.api.RealRecipeApi.dislike
-import tupperdate.api.RealRecipeApi.like
 import kotlin.math.absoluteValue
 import tupperdate.android.R
+import tupperdate.android.ui.TupperdateTypography
+import tupperdate.api.RealRecipeApi.dislike
+import tupperdate.api.RealRecipeApi.like
+import tupperdate.api.RecipeApi
+
+var presentRecipe= getNewRecipe()
 
 @Composable
 fun Home(
     onChatClick: () -> Unit,
     onProfileClick: () -> Unit,
-    onLike: () -> Unit,
-    onDislike: () -> Unit,
     onReturn: () -> Unit,
     onRecipeClick: () -> Unit
 ) {
     Scaffold(
         topBar = { mainTopBar(onChatClick, onProfileClick) },
-        bodyContent = { defaultContent(onLike, onDislike) },
-        bottomBar = { mainBottomBar(onLike, onDislike, onReturn, onRecipeClick) }
+        bodyContent = { defaultContent() },
+        bottomBar = { mainBottomBar({ like(presentRecipe) },
+            { dislike(presentRecipe) }, onReturn, onRecipeClick) }
     )
 }
 
 @Composable
-fun blueBox(offset: Int, modifier: Modifier = Modifier) {
-    Box(
-        modifier = Modifier.size(boxContentSize.dp).offset(x = offset.dp)
-            // TODO find new parameter name for shape
-            //shape = RoundedCornerShape(8.dp),
-            .background(Color.Cyan), alignment = Alignment.Center
-    ) {
-        Image(
-            imageResource(id = R.drawable.redlobtser),
-            Modifier.fillMaxWidth(),
-            contentScale = ContentScale.FillWidth
-        )
-        Text(text = "Red lobster")
-    }
-}
-
-@Composable
 fun defaultContent(
-    onLike: () -> Unit,
-    onDislike: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Row(Modifier.fillMaxSize()) {
         Column(Modifier.fillMaxWidth().align(Alignment.CenterVertically)) {
             val (pos, setPos) = remember { mutableStateOf(0) }
-            Box(modifier = Modifier.size(boxContentSize.dp).align(Alignment.CenterHorizontally)
+
+            Box(modifier = Modifier.width(boxContentWidth.dp).height(boxContentHeight.dp)
+                .align(Alignment.CenterHorizontally)
                 .draggable(Orientation.Horizontal,
-                    onDragStarted = {
-                        //Toast.makeText(applicationContext, "It really works", Toast.LENGTH_SHORT).show()
-                    }, onDrag =
+                    onDragStarted = {}, onDrag =
                     { delta ->
                         run {
                             setPos(pos + (delta * 0.75).toInt())
@@ -87,14 +69,15 @@ fun defaultContent(
             {
                 if (pos.absoluteValue > swipeMargin) {
                     if (pos > 0) {
-                        onLike
+                        like(presentRecipe)
                     } else if (pos < 0) {
-                        onDislike
+                        dislike(presentRecipe)
                     }
                     setPos(0)
-                    blueBox(pos)
+                    presentRecipe=(getNewRecipe())
+                    recipeCard(pos, presentRecipe) // TODO : get a new recipe and pass it to the box
                 } else {
-                    blueBox(pos)
+                    recipeCard(pos, presentRecipe)
                 }
             }
         }
@@ -109,8 +92,6 @@ private fun HomeDisconnectPreview() {
         Home(
             onChatClick = {},
             onProfileClick = {},
-            onLike = {},
-            onDislike = {},
             onRecipeClick = {},
             onReturn = {}
         )
@@ -118,5 +99,6 @@ private fun HomeDisconnectPreview() {
 }
 
 const val TAG: String = "Main"
-const val boxContentSize: Int = 300
+const val boxContentWidth: Int = 300
+const val boxContentHeight: Int = 525
 const val swipeMargin: Int = 200
---
+++ src/mobile/src/main/java/tupperdate/android/home/RecipeCard.kt
@@ -0,0 +1,54 @@
+package tupperdate.android.home
+
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import tupperdate.android.R
+import tupperdate.android.ui.TupperdateTypography
+import tupperdate.api.RecipeApi
+
+@Composable
+fun recipeCard(offset: Int, recipe: RecipeApi.Recipe) {
+    Card(
+        modifier = Modifier.fillMaxSize().offset(x = offset.dp),
+        backgroundColor = (Color.Cyan),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Box(Modifier.fillMaxSize())
+        {
+            Image(
+                // TODO modify parameter to use a String url
+                imageResource(id = R.drawable.lobster),
+                Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillHeight
+            )
+            Row(
+                Modifier.fillMaxWidth().align(Alignment.BottomStart)
+                .padding(5.dp)) {
+                Column(Modifier.width((boxContentWidth*0.75).dp)) {
+                    Text(recipe.title, style = TupperdateTypography.body1)
+                    Text(recipe.description, style = TupperdateTypography.body2)
+                }
+                Column(
+                    Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.Top) {
+                    IconButton(onClick = {}) {
+                        Icon(vectorResource(id = R.drawable.ic_help_outline_white_18dp))
+                    }
+                }
+            }
+        }
+    }
+}
