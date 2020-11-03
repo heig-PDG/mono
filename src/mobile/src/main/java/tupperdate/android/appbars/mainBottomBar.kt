669f5ef8fb21fbf75aeef16b302d01a631953023
David <duprazdavid@gmail.com>
2020-11-03 20:56:41 +0100

    Fix imports

--- src/mobile/src/main/java/tupperdate/android/appbars/mainBottomBar.kt
+++ src/mobile/src/main/java/tupperdate/android/appbars/mainBottomBar.kt
@@ -4,15 +4,10 @@ import androidx.compose.foundation.Icon
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.dp
 import tupperdate.android.R
 import tupperdate.android.home.BarButton
 import tupperdate.android.home.littleButtonSize
