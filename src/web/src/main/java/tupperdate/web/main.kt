d1bb2051273f322653306d4706f517fc0dfd7dbe
matt989253 <matthieu.burguburu@gmail.com>
2020-11-03 16:27:11 +0100

    Setup Firebase Realtime Database for local configuration

--- src/web/src/main/java/tupperdate/web/main.kt
+++ src/web/src/main/java/tupperdate/web/main.kt
@@ -2,13 +2,22 @@
 
 package tupperdate.web
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import tupperdate.common.helloWorld
+import java.io.FileInputStream
+import java.nio.file.Paths
+
 
 private const val DefaultPort = 1234
 private const val DefaultPortEnvVariable = "PORT"
@@ -18,11 +27,33 @@ fun main() {
     val port = System.getenv(DefaultPortEnvVariable)?.toIntOrNull() ?: DefaultPort
 
     val server = embeddedServer(Netty, port = port) {
+        // Firebase options
+        val serviceAccount = FileInputStream("web\\tupperdate-developement-serice-account-firebase-admin-key.json")
+
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setDatabaseUrl("https://tupperdate-developement.firebaseio.com")
+            .build()
+
+        // Initialise FirebaseApp
+        FirebaseApp.initializeApp(options)
+
+        val ref = FirebaseDatabase.getInstance()
+            .getReference("test")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val document = dataSnapshot.value
+                println(document)
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+
         install(DefaultHeaders)
         install(CallLogging)
         install(Routing) {
             get("/") {
-                call.respondText(helloWorld())
+                call.respondText("Hello world")
             }
         }
     }
