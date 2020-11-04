9af69c410c9c12ae4d7795696854569de2bab58d
Alexandre Piveteau <alexandre@piveteau.email>
2020-11-04 15:06:49 +0100

    Use environment variables for the Firebase Admin SDK
    Co-authored-by: Matthieu Burguburu <matthieu.burguburu@gmail.com>

--- src/web/src/main/java/tupperdate/web/main.kt
+++ src/web/src/main/java/tupperdate/web/main.kt
@@ -12,39 +12,51 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import java.io.FileInputStream
 
 private const val DefaultPort = 1234
 private const val DefaultPortEnvVariable = "PORT"
+private const val DefaultGoogleServiceAccountEnvVariable = "GOOGLE_SERVICE_ACCOUNT"
+private const val DefaultGoogleDatabaseName = "GOOGLE_DATABASE_NAME"
+
+/**
+ * Retrieves the service account from the environment variables and returns the associated
+ * [GoogleCredentials]. If no credential could be built, and exception will be thrown.
+ */
+private fun retrieveServiceAccount(): GoogleCredentials {
+    return requireNotNull(
+        System.getenv(DefaultGoogleServiceAccountEnvVariable)
+            .byteInputStream()
+            .let(GoogleCredentials::fromStream)
+    ) { "Missing \$$DefaultGoogleServiceAccountEnvVariable environment variable." }
+}
 
 @JvmName("main")
 fun main() {
     val port = System.getenv(DefaultPortEnvVariable)?.toIntOrNull() ?: DefaultPort
+    val database = requireNotNull(System.getenv(DefaultGoogleDatabaseName)) {
+        "Missing \$$DefaultGoogleDatabaseName environment variable."
+    }
+
+    val options = FirebaseOptions.builder()
+        .setCredentials(retrieveServiceAccount())
+        .setDatabaseUrl(database)
+        .build()
+
+    // Initialise FirebaseApp
+    FirebaseApp.initializeApp(options)
+    val firestore = FirestoreClient.getFirestore()
 
     val server = embeddedServer(Netty, port = port) {
-        // Firebase options
-        val serviceAccount = FileInputStream("web\\tupperdate-developement-service-account-firebase-admin-key.json")
-
-        val options = FirebaseOptions.builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .setDatabaseUrl("https://tupperdate-developement.firebaseio.com")
-            .build()
-
-        // Initialise FirebaseApp
-        FirebaseApp.initializeApp(options)
-
-        val firestore = FirestoreClient.getFirestore()
-        val testCollection = firestore.collection("test")
-        val data = hashMapOf(
-            "hello" to "there",
-            "general" to "kenobi"
-        )
-        testCollection.document("greetings").set(data as Map<String, Any>)
-
         install(DefaultHeaders)
         install(CallLogging)
         install(Routing) {
             get("/") {
+                val testCollection = firestore.collection("test")
+                val data = hashMapOf(
+                    "hello" to "there",
+                    "general" to "kenobi"
+                )
+                testCollection.document("greetings").set(data as Map<String, Any>)
                 call.respondText("Hello world")
             }
         }
