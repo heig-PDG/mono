71eea03e3b777889833db6ba7c4eb0be8d095a16
Alexandre Piveteau <alexandre@piveteau.email>
2020-11-22 21:50:47 +0100

    Simplify RealApi

--- src/mobile/src/api/java/tupperdate/api/RealApi.kt
+++ src/mobile/src/api/java/tupperdate/api/RealApi.kt
@@ -4,13 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class RealApi(activity: AppCompatActivity) : Api {
-    override val authentication: RealAuthenticationApi
-    override val recipe: RealRecipeApi
-    override val images: ImagePickerApi
-
-    init {
-        authentication = RealAuthenticationApi(activity, FirebaseAuth.getInstance())
-        recipe = RealRecipeApi(authentication)
-        images = ActualImagePickerApi(activity)
-    }
+    override val authentication = RealAuthenticationApi(activity, FirebaseAuth.getInstance())
+    override val recipe = RealRecipeApi(authentication)
+    override val images = ActualImagePickerApi(activity)
 }
