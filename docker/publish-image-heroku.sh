0ca840d1d9f40d5492467b4966c32deecba60254
Alexandre Piveteau <alexandre@piveteau.email>
2020-11-22 21:23:54 +0100

    Try to set up SwaggerUI deployment to Heroku

--- docker/publish-image-heroku.sh
+++ docker/publish-image-heroku.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 docker tag tupperdate-web registry.heroku.com/heig-pdg/web
+docker tag tupperdate-api registry.heroku.com/heig-pdg-api/web
 
-# Log in to GitHub package registry.
+# Push heig-pdg image.
 echo $TOKEN_HEROKU | docker login registry.heroku.com --username=_ --password-stdin
 
-# Push the Docker images.
+# Push both container images.
 docker push registry.heroku.com/heig-pdg/web
+docker push registry.heroku.com/heig-pdg-api/web
