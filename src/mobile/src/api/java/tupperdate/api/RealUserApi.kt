f181d9944e22ce1011fae59bc3e3f6090cb2a12f
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-11-23 20:38:21 +0100

    Fix bug in sending profile

--- src/mobile/src/api/java/tupperdate/api/RealUserApi.kt
+++ src/mobile/src/api/java/tupperdate/api/RealUserApi.kt
@@ -4,7 +4,9 @@ import android.util.Log
 import io.ktor.client.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import tupperdate.common.dto.MyUserDTO
 import tupperdate.common.dto.UserDTO
 
@@ -13,30 +15,37 @@ class RealUserApi(
     private val uid: Flow<String?>,
 ) : UserApi {
 
-    override fun profile(): Flow<UserApi.Profile?>
-        = uid.map { uid ->
-                try {
-                    val currentProfile = client.get<UserDTO>("/users/$uid")
+    override fun profile(): Flow<UserApi.Profile?> = flow {
+        val currentUid = uid.filterNotNull().first()
+        var profile:UserApi.Profile? = null
 
-                    return@map UserApi.Profile(
-                        displayName = currentProfile.displayName,
-                        profileImageUrl = currentProfile.picture,
-                    )
-                } catch (t: Throwable) {
-                    Log.d("UserDebug", t.message.toString())
-                    return@map null
-                }
-            }
+        try {
+            val currentProfile = client.get<UserDTO>("/users/$currentUid")
+
+            Log.d("UserDebug", currentProfile.displayName)
+
+            profile = UserApi.Profile(
+                displayName = currentProfile.displayName,
+                profileImageUrl = currentProfile.picture,
+            )
+        } catch (t: Throwable) {
+            Log.d("UserDebug", t.message.toString())
+        }
+
+        emit(profile)
+    }
 
     override suspend fun updateProfile(name: String) {
-        uid.map { uid ->
-            try {
-            client.put<UserDTO>("/users/$uid") {
+        val currentUid = uid.filterNotNull().first()
+
+        try {
+            Log.d("UserDebug", "uid: $currentUid")
+            val result = client.put<UserDTO>("/users/$currentUid") {
                 body = MyUserDTO(name)
             }
-            } catch (t: Throwable) {
-                Log.d("UserDebug", t.message.toString())
-            }
+            Log.d("UserDebug", "name: $result.displayName")
+        } catch (t: Throwable) {
+            Log.d("UserDebug", t.message.toString())
         }
     }
 
