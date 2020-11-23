25c842977f3e322eb7c3de82d9f122259100c250
matt989253 <matthieu.burguburu@gmail.com>
2020-11-23 11:23:24 +0100

    Apply refactoring to recipesPost

--- src/web/src/main/java/tupperdate/web/routing/Recipes.post.kt
+++ src/web/src/main/java/tupperdate/web/routing/Recipes.post.kt
@@ -7,9 +7,10 @@ import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import tupperdate.common.dto.NewRecipeDTO
-import tupperdate.common.dto.RecipeAttributesDTO
 import tupperdate.common.dto.RecipeDTO
 import tupperdate.web.auth.firebaseAuthPrincipal
+import tupperdate.web.model.toRecipe
+import tupperdate.web.model.toRecipeDTO
 import tupperdate.web.util.await
 
 /**
@@ -20,37 +21,10 @@ import tupperdate.web.util.await
 fun Route.recipesPost(store: Firestore) = post {
     val uid = requireNotNull(call.firebaseAuthPrincipal?.uid)
     val doc = store.collection("users").document(uid).collection("recipes").document()
-    val dto = call.receive<NewRecipeDTO>()
-    val now = System.currentTimeMillis() / 1000
+    val recipe = call.receive<NewRecipeDTO>().toRecipe(doc.id, "https://thispersondoesnotexist.com/image")
 
     // TODO (alex) : See how we can handle exceptions.
-    // TODO (matt) : See how we can avoid code duplication.
-    doc.set(
-        mapOf(
-            "id" to doc.id,
-            "title" to dto.title,
-            "description" to dto.description,
-            "timestamp" to now,
-            "attributes" to mapOf(
-                "hasAllergens" to dto.attributes.hasAllergens,
-                "vegetarian" to dto.attributes.vegetarian,
-                "warm" to dto.attributes.warm,
-            )
-        )
-    ).await()
+    doc.set(recipe).await()
 
-    call.respond(
-        HttpStatusCode.OK, RecipeDTO(
-            id = doc.id,
-            title = dto.title,
-            timestamp = now,
-            attributes = RecipeAttributesDTO(
-                hasAllergens = dto.attributes.hasAllergens,
-                vegetarian = dto.attributes.vegetarian,
-                warm = dto.attributes.warm,
-            ),
-            description = dto.description,
-            picture = "https://thispersondoesnotexist.com/image",
-        )
-    )
+    call.respond(HttpStatusCode.OK, recipe.toRecipeDTO())
 }
