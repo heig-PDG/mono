f718d0ff02cab16e07d37fb77cb71776ca25d351
matt989253 <matthieu.burguburu@gmail.com>
2020-11-25 11:33:55 +0100

    Add a logout endpoint and check refresh token validity when authorizing a ID token

---
+++ src/web/src/main/java/tupperdate/web/routing/Logout.kt
@@ -0,0 +1,27 @@
+package tupperdate.web.routing
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import tupperdate.web.auth.firebaseAuthPrincipal
+import tupperdate.web.exceptions.statusException
+
+/**
+ *
+ */
+
+fun Route.account(auth: FirebaseAuth) {
+    route("/account") {
+        logout(auth)
+    }
+}
+
+fun Route.logout(auth: FirebaseAuth) = post("/logout") {
+    val uid = call.firebaseAuthPrincipal?.uid ?: statusException(HttpStatusCode.Unauthorized)
+    auth.revokeRefreshTokens(uid)
+
+    call.respond(HttpStatusCode.OK)
+}
