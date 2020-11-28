7570b598a96700c7acc2828e6cac8d4120b21d44
David <duprazdavid@gmail.com>
2020-11-28 10:21:10 +0100

    Corrections

--- src/mobile/src/main/java/tupperdate/android/appbars/ChatBars.kt
+++ src/mobile/src/main/java/tupperdate/android/appbars/ChatBars.kt
@@ -1,12 +1,12 @@
 package tupperdate.android.appbars
 
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -53,7 +53,7 @@ fun ChatTopBar(
 }
 
 @Composable
-fun bottomBar(
+fun BottomBar(
     onSendClick:()->Unit,
     modifier: Modifier=Modifier
 ) {
