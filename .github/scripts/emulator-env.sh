fbbf07f3a765ea1b3fd5d1d86f09a81e90207809
matt989253 <matthieu.burguburu@gmail.com>
2021-01-04 17:08:00 +0100

    Export more env variables

--- .github/scripts/emulator-env.sh
+++ .github/scripts/emulator-env.sh
@@ -3,4 +3,8 @@
 echo "#!/bin/sh"
 echo "export PATH=${JAVA_HOME}:\$PATH"
 echo "export JAVA_HOME=${JAVA_HOME}"
+echo "export GOOGLE_SERVICE_ACCOUNT=${GOOGLE_SERVICE_ACCOUNT}"
+echo "export GOOGLE_DATABASE_NAME=${GOOGLE_DATABASE_NAME}"
+echo "export GOOGLE_BUCKET_NAME=${GOOGLE_BUCKET_NAME}"
+echo "export FIREBASE_TOKEN=${FIREBASE_TOKEN}"
 echo "./gradlew :web:test"
