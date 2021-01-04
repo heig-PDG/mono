aedf321245e01cc0d7331a5ff01d4fff2f03940f
matt989253 <matthieu.burguburu@gmail.com>
2021-01-04 18:02:43 +0100

    Investigate json problems

--- .github/scripts/emulator-env.sh
+++ .github/scripts/emulator-env.sh
@@ -3,7 +3,7 @@
 echo "#!/bin/sh"
 echo "export PATH=${JAVA_HOME}:\$PATH"
 echo "export JAVA_HOME=${JAVA_HOME}"
-echo "export GOOGLE_SERVICE_ACCOUNT={\"host\":\"1234\"}"
+echo "export GOOGLE_SERVICE_ACCOUNT=\$(pwd)"
 echo "export GOOGLE_DATABASE_NAME=${GOOGLE_DATABASE_NAME}"
 echo "export GOOGLE_BUCKET_NAME=${GOOGLE_BUCKET_NAME}"
 echo "export FIREBASE_TOKEN=${FIREBASE_TOKEN}"
