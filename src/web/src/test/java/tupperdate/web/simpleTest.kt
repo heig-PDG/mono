6ef8cc8f1086fd6a6e4c678c41d78940a634fbd0
matt989253 <matthieu.burguburu@gmail.com>
2021-01-04 19:30:19 +0100

    Test emulator

--- src/web/src/test/java/tupperdate/web/simpleTest.kt
+++ src/web/src/test/java/tupperdate/web/simpleTest.kt
@@ -1,22 +1,36 @@
 package tupperdate.web
 
+import com.google.firebase.cloud.FirestoreClient
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import kotlinx.coroutines.runBlocking
 import tupperdate.web.util.*
 import kotlin.test.*
+
+
+// Initialise FirebaseApp
+val firebase = initialiseApp()
+val firestore = FirestoreClient.getFirestore(firebase)
 
 class SimpleTest {
     @Test
     fun testUnauthorized() {
-        // Initialise FirebaseApp
-        val firebase = initialiseApp()
-
         withTestApplication {
             application.installServer(firebase)
 
             with(handleRequest(HttpMethod.Get, "/recipes")) {
                 assertEquals(HttpStatusCode.Unauthorized, response.status())
             }
+        }
+    }
+
+    @Test
+    fun testEmulator() {
+        val doc = firestore.collection("emulatorOnlyDeleteMe").document("uniqueId")
+        runBlocking {
+            doc.set(mapOf("displayName" to "Emulatron3000")).await()
+            val name = doc.get().await().get("displayName") as String
+            assertEquals(name, "Emulatron3000")
         }
     }
 }
