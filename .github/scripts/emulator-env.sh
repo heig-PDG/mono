fa49c046c11e7ff09c68a4faeb54cd9ed2af125c
matt989253 <matthieu.burguburu@gmail.com>
2021-01-04 17:24:30 +0100

    Export google service account into json file

--- .github/scripts/emulator-env.sh
+++ .github/scripts/emulator-env.sh
@@ -3,7 +3,7 @@
 echo "#!/bin/sh"
 echo "export PATH=${JAVA_HOME}:\$PATH"
 echo "export JAVA_HOME=${JAVA_HOME}"
-echo "export GOOGLE_SERVICE_ACCOUNT=${GOOGLE_SERVICE_ACCOUNT}"
+echo "export GOOGLE_SERVICE_ACCOUNT=\$(cat ../.github/scripts/google-service-account.json)"
 echo "export GOOGLE_DATABASE_NAME=${GOOGLE_DATABASE_NAME}"
 echo "export GOOGLE_BUCKET_NAME=${GOOGLE_BUCKET_NAME}"
 echo "export FIREBASE_TOKEN=${FIREBASE_TOKEN}"
