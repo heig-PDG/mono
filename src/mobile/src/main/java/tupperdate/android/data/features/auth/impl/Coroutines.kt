340e9277dfa2aa24f4639dda737eed675d5fcfef
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-05 13:37:14 +0100

    Make safeOffer extension method public

--- src/mobile/src/main/java/tupperdate/android/data/features/auth/impl/Coroutines.kt
+++ src/mobile/src/main/java/tupperdate/android/data/features/auth/impl/Coroutines.kt
@@ -3,11 +3,11 @@ package tupperdate.android.data.features.auth.impl
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import tupperdate.android.data.InternalDataApi
+import tupperdate.android.data.safeOffer
 
 /**
  * Returns a [Flow] of the current [FirebaseUser], and emits one on every authentication change.
@@ -22,14 +22,3 @@ val FirebaseAuth.currentUserFlow: Flow<FirebaseUser?>
         this@currentUserFlow.addAuthStateListener(callback)
         awaitClose { this@currentUserFlow.removeAuthStateListener((callback)) }
     }
-
-/**
- * Safely offers an item in a [SendChannel]. If the [SendChannel] is closed, no exception will be
- * thrown. This might be useful in scenarios where recomposition happens while an operation is
- * being performed.
- *
- * TODO : Make this API public.
- */
-private fun <T> SendChannel<T>.safeOffer(element: T): Boolean {
-    return kotlin.runCatching { offer(element) }.getOrDefault(false)
-}
