6d22927cfae8c51319b3b4f38fb7847c4520dcf4
matt989253 <matthieu.burguburu@gmail.com>
2021-01-05 21:25:35 +0100

    Implement endpoint to get specific conversation

--- src/web/src/main/java/tupperdate/web/routing/chats/Chats.conversation.kt
+++ src/web/src/main/java/tupperdate/web/routing/chats/Chats.conversation.kt
@@ -1,14 +1,61 @@
 package tupperdate.web.routing.chats
 
 import com.google.cloud.firestore.Firestore
+import com.google.cloud.firestore.Query
 import com.google.firebase.auth.FirebaseAuth
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import tupperdate.common.dto.ConversationDTO
 import tupperdate.web.auth.firebaseAuthPrincipal
 import tupperdate.web.exceptions.statusException
+import tupperdate.web.model.*
+import tupperdate.web.util.await
 
 fun Route.getConvs(store: Firestore) = get("/{userId}") {
-    call.respond(HttpStatusCode.NotImplemented)
+    val uid = call.firebaseAuthPrincipal?.uid ?: statusException(HttpStatusCode.Unauthorized)
+    val userId = call.parameters["userId"] ?: statusException(HttpStatusCode.BadRequest)
+
+    val docId = minOf(uid, userId) + "_" + maxOf(uid, userId)
+
+    val chat = store.collection("chats").document(docId).get().await().toObject(Chat::class.java) ?: statusException(HttpStatusCode.NotFound)
+
+    if (chat.user1Recipes == null || chat.user2Recipes == null) {
+        statusException(HttpStatusCode.NotFound)
+    }
+
+    val myRecipes = if (uid == chat.userId1) chat.user1Recipes else chat.user2Recipes
+    val theirRecipes = if (uid != chat.userId1) chat.user1Recipes else chat.user2Recipes
+
+    val conv = Conv(
+        id = docId,
+        myId = uid,
+        theirId = userId,
+        myRecipes = myRecipes,
+        theirRecipes = theirRecipes,
+    )
+
+    val user = store.collection("users").document(userId).get().await()
+        .toObject(User::class.java) ?: statusException(HttpStatusCode.InternalServerError)
+    val recipes = store.collection("recipes")
+
+    // TODO: Agree with EVERYONE on what to do where with missing values
+    val convDTO = ConversationDTO(
+        userId = userId,
+        displayName = user.displayName ?: "No name",
+        picture = user.picture ?: "No picture",
+        lastMessage = store.collection("chats/${docId}/messages")
+            .orderBy("timestamp", Query.Direction.DESCENDING).limit(1).get().await()
+            .toObjects(Message::class.java).getOrNull(0)?.toMessageDTO(),
+        myRecipes = conv.myRecipes.map {
+            recipes.document(conv.myId).get().await().toObject(Recipe::class.java) ?: statusException(HttpStatusCode.InternalServerError)
+        }.map { it.toRecipeDTO() },
+        theirRecipes = conv.theirRecipes.map {
+            recipes.document(conv.theirId).get().await().toObject(Recipe::class.java)
+                ?: statusException(HttpStatusCode.InternalServerError)
+        }.map { it.toRecipeDTO() },
+    )
+
+    call.respond(convDTO)
 }
