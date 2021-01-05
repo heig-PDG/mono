2bed5cf831139f236c42d1fd716d49df72a346c0
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-05 13:41:23 +0100

    Improve ambients

--- src/mobile/src/main/java/tupperdate/android/ui/ambients/Api.kt
+++ src/mobile/src/main/java/tupperdate/android/ui/ambients/Api.kt
@@ -6,11 +6,21 @@ import tupperdate.android.data.features.auth.AuthenticationStatus
 import tupperdate.android.data.features.auth.PhoneRegistrationApi
 import tupperdate.android.data.features.picker.ImagePicker
 
+/**
+ * An ambient that provides a [PhoneRegistrationApi].
+ */
 val AmbientPhoneRegistration: ProvidableAmbient<PhoneRegistrationApi> =
     ambientOf { error("not provided") }
 
+/**
+ * An ambient that provides an [ImagePicker].
+ */
 val AmbientImagePicker: ProvidableAmbient<ImagePicker> =
     ambientOf { error("not provided") }
 
+/**
+ * An ambient that provides the current [AuthenticationStatus]. If the status could not be loaded,
+ * a null value will be returned.
+ */
 val AmbientProfile: ProvidableAmbient<AuthenticationStatus?> =
-    ambientOf { error("not provided") }
+    ambientOf { null }
