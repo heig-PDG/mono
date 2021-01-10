a16df1adb007bbf26e92876e2ddd5dc45d8d1d75
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-10 22:06:17 +0100

    Fix server returning recipes of the current user in the stack

--- src/mobile/src/main/java/tupperdate/android/data/Koin.kt
+++ src/mobile/src/main/java/tupperdate/android/data/Koin.kt
@@ -6,18 +6,25 @@ import io.ktor.client.*
 import io.ktor.client.features.*
 import io.ktor.client.features.auth.*
 import io.ktor.client.features.json.*
+import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.util.*
 import org.koin.dsl.module
 import tupperdate.android.data.features.auth.firebase.firebase
 import tupperdate.android.data.room.TupperdateDatabase
 
 @InternalDataApi
+@OptIn(KtorExperimentalAPI::class)
 private val KoinKtorModule = module {
     single {
         HttpClient {
             install(Auth) { firebase() }
             install(JsonFeature)
+            install(Logging) {
+                level = LogLevel.HEADERS
+                logger = Logger.SIMPLE
+            }
             defaultRequest {
                 // TODO : Use HTTPS.
                 host = "api.tupperdate.me"
