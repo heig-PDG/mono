5ad17b2cf4c52d9c9bff7137167c8841e90209d1
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-12 02:06:18 +0100

    Sort conversations by most recent timestamp

--- src/mobile/src/main/java/tupperdate/android/ui/home/chats/ConversationsViewModel.kt
+++ src/mobile/src/main/java/tupperdate/android/ui/home/chats/ConversationsViewModel.kt
@@ -2,7 +2,7 @@ package tupperdate.android.ui.home.chats
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.map
 import tupperdate.android.data.features.messages.Conversation
 import tupperdate.android.data.features.messages.Match
 import tupperdate.android.data.features.messages.MessagesRepository
@@ -14,6 +14,7 @@ class ConversationsViewModel(
     messages: MessagesRepository,
 ) : ViewModel() {
 
-    val matches: Flow<List<Match>> = messages.matches.onEach { println("Display ${it.size} items") }
+    val matches: Flow<List<Match>> = messages.matches
     val conversations: Flow<List<Conversation>> = messages.conversations
+        .map { it.sortedByDescending { conv -> conv.previewTimestamp } }
 }
