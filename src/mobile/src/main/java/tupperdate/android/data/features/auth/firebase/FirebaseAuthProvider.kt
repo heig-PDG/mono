efbac303f20f6729ecf988bd13e3871f90c17ee8
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-12 21:44:51 +0100

    Hide API token in release mode

--- src/mobile/src/main/java/tupperdate/android/data/features/auth/firebase/FirebaseAuthProvider.kt
+++ src/mobile/src/main/java/tupperdate/android/data/features/auth/firebase/FirebaseAuthProvider.kt
@@ -9,6 +9,7 @@ import io.ktor.http.auth.*
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.tasks.await
+import tupperdate.android.BuildConfig
 import tupperdate.android.data.InternalDataApi
 import tupperdate.android.data.features.auth.AuthenticationRepository
 
@@ -32,11 +33,12 @@ private class AuthProvider(
         val token = auth.currentUserFlow.filterNotNull().first()
             .getIdToken(false).await().token
 
-        // TODO : Remove or strip this in production.
-        Log.d(
-            "AuthProvider",
-            "Authenticating ${request.method} request to ${request.url.buildString()} with $token",
-        )
+        if (BuildConfig.DEBUG) {
+            Log.d(
+                "AuthProvider",
+                "Authenticating ${request.method} request to ${request.url.buildString()} with $token",
+            )
+        }
 
         request.header(HttpHeaders.Authorization, "Bearer $token")
     }
