f548dc379c2c736c2c096189fceeff399a59ef2b
matt989253 <matthieu.burguburu@gmail.com>
2021-01-14 18:06:12 +0100

    Try to fix injection problems

--- src/web/src/main/java/tupperdate/web/main.kt
+++ src/web/src/main/java/tupperdate/web/main.kt
@@ -2,9 +2,7 @@
 
 package tupperdate.web
 
-import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.cloud.FirestoreClient
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.features.*
@@ -12,47 +10,23 @@ import io.ktor.routing.*
 import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import org.koin.dsl.module
-import org.koin.ktor.ext.Koin
+import org.koin.core.context.startKoin
 import org.koin.ktor.ext.get
-import org.koin.ktor.ext.inject
-import tupperdate.web.facade.chats.KoinModuleFacadeChat
 import tupperdate.web.facade.profiles.KoinModuleFacadeProfile
-import tupperdate.web.facade.recipes.KoinModuleFacadeRecipe
 import tupperdate.web.legacy.auth.firebase
 import tupperdate.web.legacy.routing.accounts.accounts
 import tupperdate.web.legacy.routing.chats.chats
 import tupperdate.web.legacy.routing.recipes.recipes
 import tupperdate.web.legacy.util.getPort
-import tupperdate.web.legacy.util.initialiseApp
-import tupperdate.web.model.chats.firestore.KoinModuleModelChatsFirestore
 import tupperdate.web.model.impl.firestore.KoinModuleModelFirebase
 import tupperdate.web.model.profiles.firestore.KoinModuleModelUsersFirestore
-import tupperdate.web.model.recipes.firestore.KoinModuleModelRecipesFirestore
-import java.io.ByteArrayInputStream
-import java.io.InputStream
 
 @JvmName("main")
 fun main() {
     // Retrieve the port
     val port = getPort()
 
-    val server = embeddedServer(Netty, port = port) {
-        installServer()
-    }
-
-    server.start(wait = true)
-}
-
-fun Application.installServer() {
-
-    install(DefaultHeaders)
-    install(CallLogging)
-    install(ContentNegotiation) {
-        json()
-    }
-
-    install(Koin) {
+    startKoin {
         modules(KoinModuleModelFirebase)
 
         modules(KoinModuleFacadeProfile)
@@ -62,6 +36,20 @@ fun Application.installServer() {
         modules(KoinModuleModelUsersFirestore)
         //modules(KoinModuleModelRecipesFirestore)
         //modules(KoinModuleModelChatsFirestore)
+    }
+
+    val server = embeddedServer(Netty, port = port) {
+        installServer()
+    }
+
+    server.start(wait = true)
+}
+
+fun Application.installServer() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        json()
     }
 
     install(Authentication) {
--- src/web/src/test/java/tupperdate/web/simpleTest.kt
+++ src/web/src/test/java/tupperdate/web/simpleTest.kt
@@ -1,42 +1,52 @@
 package tupperdate.web
 
 import com.google.cloud.firestore.Firestore
-import com.google.firebase.FirebaseApp
-import com.google.firebase.cloud.FirestoreClient
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlinx.coroutines.runBlocking
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.ktor.ext.inject
+import tupperdate.web.facade.profiles.KoinModuleFacadeProfile
 import tupperdate.web.legacy.util.*
+import tupperdate.web.model.impl.firestore.KoinModuleModelFirebase
+import tupperdate.web.model.profiles.firestore.KoinModuleModelUsersFirestore
 import kotlin.test.*
 
-// Initialise FirebaseApp
+fun testApp(test: TestApplicationEngine.() -> Unit) {
+    withTestApplication {
+        stopKoin() // Need to stop koin and restart after other tests
+        startKoin {
+            modules(KoinModuleModelFirebase)
+            modules(KoinModuleFacadeProfile)
+            modules(KoinModuleModelUsersFirestore)
+        }
+
+        test() // Run you application
+    }
+}
+
 
 class SimpleTest {
 
     @Test
-    fun testUnauthorized() {
-        withTestApplication {
-            application.installServer()
-
-            with(handleRequest(HttpMethod.Get, "/recipes")) {
-                assertEquals(HttpStatusCode.Unauthorized, response.status())
-            }
+    fun testUnauthorized() = testApp {
+        application.installServer()
+        with(handleRequest(HttpMethod.Get, "/recipes")) {
+            assertEquals(HttpStatusCode.Unauthorized, response.status())
         }
     }
 
     @Test
-    fun testEmulator() {
-        withTestApplication {
-            application.installServer()
-            val firestore by application.inject<Firestore>()
+    fun testEmulator() = testApp {
+        application.installServer()
+        val firestore by application.inject<Firestore>()
 
-            val doc = firestore.collection("emulatorOnlyDeleteMe").document("uniqueId")
-            runBlocking {
-                doc.set(mapOf("displayName" to "Emulatron3000")).await()
-                val name = doc.get().await().get("displayName") as String
-                assertEquals(name, "Emulatron3000")
-            }
+        val doc = firestore.collection("emulatorOnlyDeleteMe").document("uniqueId")
+        runBlocking {
+            doc.set(mapOf("displayName" to "Emulatron3000")).await()
+            val name = doc.get().await().get("displayName") as String
+            assertEquals(name, "Emulatron3000")
         }
     }
 }
