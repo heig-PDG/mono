76aff12853ef774e630d8b671d43fbb52612e457
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-15 14:44:49 +0100

    Fix #216

--- src/mobile/src/main/java/tupperdate/android/data/features/auth/work/RefreshProfileWorker.kt
+++ src/mobile/src/main/java/tupperdate/android/data/features/auth/work/RefreshProfileWorker.kt
@@ -4,18 +4,17 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.dropbox.android.external.store4.StoreBuilder
-import com.dropbox.android.external.store4.fresh
-import com.google.firebase.auth.FirebaseAuth
+import com.dropbox.android.external.store4.StoreRequest
 import io.ktor.client.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.*
 import org.koin.core.component.KoinApiExtension
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import tupperdate.android.data.InternalDataApi
-import tupperdate.android.data.features.auth.firebase.currentUserFlow
+import tupperdate.android.data.features.auth.AuthenticationRepository
+import tupperdate.android.data.features.auth.AuthenticationStatus
 import tupperdate.android.data.features.auth.store.ProfileFetcher
 import tupperdate.android.data.features.auth.store.ProfileSourceOfTruth
 import tupperdate.android.data.room.TupperdateDatabase
@@ -31,17 +30,23 @@ class RefreshProfileWorker(
     params: WorkerParameters,
 ) : CoroutineWorker(context, params), KoinComponent {
 
+    private val auth by inject<AuthenticationRepository>()
     private val client by inject<HttpClient>()
     private val database by inject<TupperdateDatabase>()
 
     override suspend fun doWork(): Result {
         return try {
-            val user = FirebaseAuth.getInstance().currentUserFlow.filterNotNull().first()
+            val user = auth.identifier.first().identifier
             val store = StoreBuilder.from(
-                ProfileFetcher(client),
+                ProfileFetcher(client, skipLoading = true),
                 ProfileSourceOfTruth(database.profiles())
             ).build()
-            store.fresh(user.uid)
+            store.stream(StoreRequest.fresh(user))
+                .map { it.dataOrNull() }
+                .filterNotNull()
+                .filterIsInstance<AuthenticationStatus.Loaded>()
+                .take(1)
+                .collect()
             Result.success()
         } catch (throwable: Throwable) {
             throwable.printStackTrace()
