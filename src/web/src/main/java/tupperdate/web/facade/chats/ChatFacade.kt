342e2211b483cafb4c3e56b54ee01e64744832d1
David <duprazdavid@gmail.com>
2021-01-16 16:10:32 +0100

    comment facades

--- src/web/src/main/java/tupperdate/web/facade/chats/ChatFacade.kt
+++ src/web/src/main/java/tupperdate/web/facade/chats/ChatFacade.kt
@@ -4,20 +4,31 @@ import tupperdate.web.model.Result
 import tupperdate.web.model.profiles.User
 
 interface ChatFacade {
-
+    /**
+     * Fetch all chats of an user
+     */
     suspend fun readAll(
         user: User,
     ): Result<List<Chat>>
 
+    /**
+     * Fetch one of all chats of an user
+     */
     suspend fun readOne(
         user: User,
     ): Result<Chat>
 
+    /**
+     * Mark all messages as "read"
+     */
     suspend fun readMessages(
         user: User,
         otherUserId: String,
     ): Result<List<Chat>>
 
+    /**
+     * Send a message to another user
+     */
     suspend fun sendMessage(
         user: User,
         otherUserId: String,
