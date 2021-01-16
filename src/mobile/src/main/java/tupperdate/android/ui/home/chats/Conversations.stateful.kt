fe8ce2708b3cdb3ef8b9073629b4f9937e3764af
Alexandre Piveteau <alexandre@piveteau.email>
2021-01-16 11:33:49 +0100

    Add empty chat screen and fix onboarding

--- src/mobile/src/main/java/tupperdate/android/ui/home/chats/Conversations.stateful.kt
+++ src/mobile/src/main/java/tupperdate/android/ui/home/chats/Conversations.stateful.kt
@@ -2,7 +2,6 @@ package tupperdate.android.ui.home.chats
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import org.koin.androidx.compose.getViewModel
 import tupperdate.android.data.features.auth.firebase.FirebaseUid
@@ -19,13 +18,19 @@ fun Conversations(
     modifier: Modifier = Modifier,
 ) {
     val viewModel = getViewModel<ConversationsViewModel>()
-    val matches by viewModel.matches.collectAsState(emptyList())
-    val conversations by viewModel.conversations.collectAsState(emptyList())
+    val matches = viewModel.matches.collectAsState(null).value
+    val conversations = viewModel.conversations.collectAsState(null).value
 
-    Conversations(
-        matches = matches,
-        conversations = conversations,
-        onConversationClick = onConversationClick,
-        modifier = modifier,
-    )
+    if (conversations != null && matches != null) {
+        if (conversations.isEmpty() && matches.isEmpty()) {
+            EmptyConversations()
+        } else {
+            Conversations(
+                matches = matches,
+                conversations = conversations,
+                onConversationClick = onConversationClick,
+                modifier = modifier,
+            )
+        }
+    }
 }
