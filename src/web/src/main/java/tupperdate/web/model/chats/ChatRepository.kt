ff5bc2460c55379a02137c25ce91d95bca8ca549
David <duprazdavid@gmail.com>
2021-01-16 17:29:44 +0100

    Comment repositories

--- src/web/src/main/java/tupperdate/web/model/chats/ChatRepository.kt
+++ src/web/src/main/java/tupperdate/web/model/chats/ChatRepository.kt
@@ -6,30 +6,48 @@ import tupperdate.web.model.profiles.User
 
 interface ChatRepository {
 
+    /**
+     * Read all chats of an user
+     */
     suspend fun readAll(
         user: User,
     ): Result<List<ModelChat>>
 
+    /**
+     * Read one chat of one user
+     */
     suspend fun readOne(
         user: User,
         otherUserId: String,
     ): Result<ModelChat>
 
+    /**
+     * Read all messages in a chat
+     */
     suspend fun readMessages(
         user: User,
         otherUserId: String,
     ): Result<List<ModelMessage>>
 
+    /**
+     * Send a message to another user
+     */
     suspend fun sendMessage(
         user: User,
         otherUserId: String,
         content: String,
     ): Result<Unit>
 
+    /**
+     * Create and register a new chat
+     */
     suspend fun saveNewChat(
         chat: ModelNewChat,
     ): Result<Unit>
 
+    /**
+     * Update new chats list according to new matches
+     */
     suspend fun updateLikes(
         chatId: String,
         likes: Map<String, String>,
--- src/web/src/main/java/tupperdate/web/model/recipes/RecipeRepository.kt
+++ src/web/src/main/java/tupperdate/web/model/recipes/RecipeRepository.kt
@@ -5,21 +5,33 @@ import tupperdate.web.model.profiles.User
 
 interface RecipeRepository {
 
+    /**
+     * Save a new recipe
+     */
     suspend fun save(
         user: User,
         recipe: ModelNewRecipe,
     ): Result<Unit>
 
+    /**
+     * Read of user's recipes
+     */
     suspend fun readOwn(
         user: User,
     ): Result<List<ModelRecipe>>
 
+    /**
+     * Read all liked recipes
+     */
     suspend fun readAll(
         user: User,
         count: Int,
         lastSeenRecipe: Long,
     ): Result<List<ModelRecipe>>
 
+    /**
+     * Read one liked recipe
+     */
     suspend fun readOne(
         user: User,
         recipeId: String,
